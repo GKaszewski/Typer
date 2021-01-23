cdede2859e389d1a540c535527a99446631048cf
Gabriel <gabrielkaszewski@gmail.com>
2021-01-23 22:21:56 +0100

    Init

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace Typer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contentTextbox = new System.Windows.Forms.RichTextBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.intervalSlider = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.intervalLabel = new System.Windows.Forms.Label();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.intervalSlider)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contentTextbox
+            // 
+            this.contentTextbox.AcceptsTab = true;
+            this.contentTextbox.BulletIndent = 4;
+            this.contentTextbox.EnableAutoDragDrop = true;
+            this.contentTextbox.Location = new System.Drawing.Point(12, 12);
+            this.contentTextbox.Name = "contentTextbox";
+            this.contentTextbox.Size = new System.Drawing.Size(593, 542);
+            this.contentTextbox.TabIndex = 0;
+            this.contentTextbox.Text = "";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(12, 560);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(93, 560);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 2;
+            this.stopButton.Text = "stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // intervalSlider
+            // 
+            this.intervalSlider.LargeChange = 10;
+            this.intervalSlider.Location = new System.Drawing.Point(407, 560);
+            this.intervalSlider.Maximum = 100;
+            this.intervalSlider.Minimum = 1;
+            this.intervalSlider.Name = "intervalSlider";
+            this.intervalSlider.Size = new System.Drawing.Size(129, 45);
+            this.intervalSlider.TabIndex = 3;
+            this.intervalSlider.TickFrequency = 5;
+            this.intervalSlider.Value = 1;
+            this.intervalSlider.Scroll += new System.EventHandler(this.intervalSlider_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 564);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Interval";
+            // 
+            // intervalLabel
+            // 
+            this.intervalLabel.AutoSize = true;
+            this.intervalLabel.Location = new System.Drawing.Point(542, 568);
+            this.intervalLabel.Name = "intervalLabel";
+            this.intervalLabel.Size = new System.Drawing.Size(13, 15);
+            this.intervalLabel.TabIndex = 5;
+            this.intervalLabel.Text = "0";
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(174, 560);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 6;
+            this.resetButton.Text = "reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(255, 560);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 606);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(617, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(35, 17);
+            this.statusLabel.Text = "Hello";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 628);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.intervalLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.intervalSlider);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.contentTextbox);
+            this.MaximumSize = new System.Drawing.Size(633, 700);
+            this.Name = "Form1";
+            this.Text = "Typer";
+            ((System.ComponentModel.ISupportInitialize)(this.intervalSlider)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox contentTextbox;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.TrackBar intervalSlider;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label intervalLabel;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace Typer
+{
+    public partial class Form1 : Form
+    {
+        private float interval = 0.0f;
+        private Queue<string> data = new Queue<string>();
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vlc);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            int startHotKeyID = 1;
+            int stopHotKeyID = 2;
+            int startHotkeyCode = (int)Keys.F5;
+            int stopHotkeyCode = (int)Keys.F6;
+            var f5Registered = RegisterHotKey(this.Handle, startHotKeyID, 0x0000, startHotkeyCode);
+            var f6Registered = RegisterHotKey(this.Handle, stopHotKeyID, 0x0000, stopHotkeyCode);
+
+            if (!f5Registered) statusLabel.Text = "Couldn't set f5 shortcut.";
+            if (!f6Registered) statusLabel.Text = "Couldn't set f6 shortcut.";
+
+            interval = getIntervalFromSecsToMs(intervalSlider.Value);
+            intervalLabel.Text = $"{interval} ms";
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312)
+            {
+                var id = m.WParam.ToInt32();
+                if (id == 1)
+                {
+                    initializeData();
+                    timer1.Start();
+                }
+
+                if (id == 2)
+                {
+                    timer1.Stop();
+                }
+            }
+
+            base.WndProc(ref m);
+        }
+
+        private float getIntervalFromSecsToMs(float val)
+        {
+            return (val / 1000.0f) * 1000.0f;
+        }
+
+        private void initializeData()
+        {
+            data.Clear();
+            var content = contentTextbox.Text;
+            foreach (var ch in content)
+            {
+                var s = Convert.ToString(ch);
+                
+                data.Enqueue(s);
+            }
+        }
+
+        private void intervalSlider_Scroll(object sender, EventArgs e)
+        {
+            interval = getIntervalFromSecsToMs(intervalSlider.Value);
+            timer1.Interval = (int)interval;
+            intervalLabel.Text = $"{interval} ms";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var result = data.TryDequeue(out string s);
+
+            if (result)
+            {
+                s = Regex.Replace(s, "[+^%~(){}]", "{$0}");
+                SendKeys.Send(s);
+            }
+            else
+            {
+                timer1.Stop();
+            }
+
+        }
+
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            initializeData();
+
+            timer1.Start();
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        { 
+            initializeData();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            contentTextbox.Text = "";
+            data.Clear();
+        }
+    }
+}
